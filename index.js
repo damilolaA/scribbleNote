102a80cbf3d376f31fa43da1ba24d438eef41579
dammy <adiodamilola@yahoo.com>
2017-12-31 17:03:27 +0100

    create config file and define port property on the config object

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-var app = require("./server/server.js"),
+var app     = require("./server/server.js"),
+	config  = require("./config/config.js"),
+	port    = config.port,
 	express = require("express");
 
 
 app.use(express.static(__dirname + "/www"));
 
-app.listen(2000, function(err) {
+app.listen(port, function(err) {
 	if(err) {return console.log(err);}
 
 	console.log("server started");
