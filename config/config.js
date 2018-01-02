a57df42b65d733243ad9b12f59484db5b46e8e60
dammy <adiodamilola@yahoo.com>
2018-01-02 21:55:39 +0100

    update database url in config file

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 var config = {
 	port: process.env.PORT || 2000,
-	database: process.env.MONGOLAB_URI || "mongodb://mongo:27017/note"
+	database: process.env.MONGOLAB_URI || "mongodb://mongo:27017/note" || "mongodb://192.168.99.100:27017/note"
 }
 
 module.exports = config;
