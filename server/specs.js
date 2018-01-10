e4f85576f69f45e804418a574d75c26c1133a631
dammy <adiodamilola@yahoo.com>
2018-01-10 22:34:52 +0100

    write proper test for the post route

--- server/specs.js
+++ server/specs.js
@@ -1,12 +1,30 @@
 var app     = require("./server.js"),
+	chai	= require("chai"),
+	assert  = chai.assert,
+	expect  = chai.expect,
+	should  = chai.should(),
+	app     = require("./server.js"),
 	request = require("supertest");
 
 
 describe("scribbleNote", function() {
 
-	it("should expect 200", function() {
+	it("should test if the post route returns data", function(done) {
+		var data = {
+			email: "shola@gmail.com",
+			password: "shola"
+		};
+
 		request(app)
-			.get("/api/v1/users")
+			.post("/api/v1/users")
+			.send(data)
+			.set("Content-Type", "Application/json")
 			.expect(200)
+			.end(function(err, res) {
+
+				expect(res.body).to.be.an("object")
+				expect(res.body.email).to.be.equal(data.email);
+				done()
+			})
 	})
 })
